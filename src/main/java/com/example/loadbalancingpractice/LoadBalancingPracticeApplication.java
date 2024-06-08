063048f138a8686d813823c1c4c1a301e41499a7
y-ngm-n <wogitopp@naver.com>
2024-06-08 19:44:37 +0900

    initialization

---
+++ src/main/java/com/example/loadbalancingpractice/LoadBalancingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loadbalancingpractice/LoadBalancingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
